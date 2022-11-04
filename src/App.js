c282ada4a7924d41ed2afcb953eae8fea04916af
unknown <tafedericoemanuel@gmail.com>
2022-11-04 00:57:39 -0300

    Home modificado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
 import Home1 from './pages/Home/Home';
-
+import Home2 from './components/Home2/Home2';
 function App() {
 
   return (
     <>
     <Home1 />
+    <Home2 />
+    
    {/*  <Main></Main> */}
    
     </>
