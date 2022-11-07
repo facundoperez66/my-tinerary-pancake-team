13e09dc252971eb924e2ece37810e31321052494
unknown <tafedericoemanuel@gmail.com>
2022-11-07 04:36:37 -0300

    detailCity

---
+++ src/components/CardEvent/CardEvent.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './CardEvent.css'
+
+export default function CardEvent(props) {
+  let { name, price, description, photo, duration } = props;
+  return (
+  <div className="contenedoralgo">
+    <div className="card-topp">
+      <img src={photo} alt="event" />
+    </div>
+    <div className="card-body">
+      <h3>
+       {name}
+      </h3>
+      <h5>
+     {description}
+      </h5>
+      <h5>USD $
+     {price}
+      </h5>
+      <h5>Duration:  
+     {duration}
+      </h5>
+    </div>
+  </div>
+ 
+  )
+}
